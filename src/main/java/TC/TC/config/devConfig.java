e713a0fc23bbd39aac66a958efa9e9e59946aadd
witor augusto <witorsantosaugusto@gmail.com>
2021-02-20 16:54:01 -0300

    Configuração e envio do projeto para produção

--- src/main/java/TC/TC/config/devConfig.java
+++ src/main/java/TC/TC/config/devConfig.java
@@ -2,6 +2,7 @@ package TC.TC.config;
 
 import TC.TC.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,8 +15,16 @@ import java.text.ParseException;
 public class devConfig {
     @Autowired
     private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String stategy;
+
     @Bean
     public boolean instatiateDatabase() throws ParseException {
+
+        if(!"create".equals(stategy)){
+            return false;
+        }
         dbService.instatiateTestDatabase();
         return true;
     }
